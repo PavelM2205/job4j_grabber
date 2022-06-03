de169a863f8b45cf8ab1f524882c4ac89ba68964
PavelM2205 <pavelm2205@mail.ru>
2022-06-03 10:44:26 +0700

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/UsageUser.java
+++ src/main/java/ru/job4j/gc/UsageUser.java
@@ -2,7 +2,7 @@ package ru.job4j.gc;
 
 public class UsageUser {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100000; i++) {
+        for (int i = 1; i <= 1000000; i++) {
             new User("User" + i, i);
         }
     }
